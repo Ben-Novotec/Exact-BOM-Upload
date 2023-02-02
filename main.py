89381b49b2d8d68a5ed36ecf3d886b55b2bcd59f
Ben-Van-Raemdonck <Ben-vanraemdonck@hotmail.com>
2023-02-02 13:25:03 +0100

    add filedialog

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import artikelen
 import stuklijst
+from tkinter import filedialog
 
 if __name__ == '__main__':
-    bom_path = 'Bill of Materials SQRF02_B voor exact.xlsx'
+    bom_path = filedialog.askopenfilename(filetypes=(('Excel', 'xlsx'),))
     steel_code = 'A4'
     artikelen.main(bom_path, steel_code)
     stuklijst.main(bom_path, steel_code)
